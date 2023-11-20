06a21be6a5fd4690dde9daa5144dc5d3224034d0
exec zsh <soledad.coding@gmail.com>
2023-11-20 16:05:27 +0100

    Booking controller over

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,40 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id, :duck_id)
+  end
+end
