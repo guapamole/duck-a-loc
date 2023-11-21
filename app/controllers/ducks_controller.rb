790644a9ef1c820daf37874411a33cd5b460954e
Sophia Choucha <sophiachoucha@gmail.com>
2023-11-21 14:15:31 +0100

    index done without the images

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,5 +1,9 @@
 class DucksController < ApplicationController
 
+  def index
+    @ducks = Duck.all
+  end
+
   def article_params
     params.require(:ducks).permit(:title, :description, :price, photos: [])
   end
